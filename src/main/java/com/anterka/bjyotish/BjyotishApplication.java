42cde77d92592e9c21120c7c200a216632518a65
TriBhaskar <panthribhaskar@gmail.com>
2025-06-16 19:18:22 +0530

    first commit

---
+++ src/main/java/com/anterka/bjyotish/BjyotishApplication.java
@@ -0,0 +1,13 @@
+package com.anterka.bjyotish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BjyotishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BjyotishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anterka/bjyotish/BjyotishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anterka.bjyotish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BjyotishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
